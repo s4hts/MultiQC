e15c55a78165a28d0d04e754459f4c96073c9eb6
Brad Jenner <bradjenner@Brads-MacBook-Pro-2.local>
2020-07-01 15:39:50 -0700

    primers bp pca stat

--- multiqc/modules/htstream/apps/Primers.py
+++ multiqc/modules/htstream/apps/Primers.py
@@ -118,6 +118,7 @@ class Primers():
 
 		for key in json.keys():
 
+			reads_lost = ( json[key]["Fragment"]["in"] - json[key]["Fragment"]["out"] ) / json[key]["Fragment"]["in"]
 			bp_lost = ( json[key]["Fragment"]["basepairs_in"] - json[key]["Fragment"]["basepairs_out"] )
 			perc_bp_lost = bp_lost / json[key]["Fragment"]["basepairs_in"]
 
@@ -125,8 +126,8 @@ class Primers():
 
 			overview_dict[key] = {
 								  "Output_Bp": json[key]["Fragment"]["basepairs_out"],
-								  "PE_reads_out": (json[key]["Paired_end"]["out"] / json[key]["Fragment"]["out"]) * 100,
-								  "SE_reads_out": (json[key]["Single_end"]["out"] / json[key]["Fragment"]["out"]) * 100,
+								  "PE_bps_out": ( (json[key]["Paired_end"]["Read1"]["basepairs_out"] + json[key]["Paired_end"]["Read2"]["basepairs_out"]) / json[key]["Fragment"]["basepairs_out"]) * 100,
+								  "SE_bps_out": (json[key]["Single_end"]["basepairs_out"] / json[key]["Fragment"]["basepairs_out"]) * 100,
 								  "Fraction_Bp_Lost": bp_lost / json[key]["Fragment"]["basepairs_in"]
 								  }
 
